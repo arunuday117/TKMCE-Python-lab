3e6725a3c093e0d462859bb024390d7d5ac8b038
Arun Uday <72249990+arunuday117@users.noreply.github.com>
2021-11-30 14:09:48 +0530

    Add files via upload

--- out2_11.py
+++ out2_11.py
@@ -2,7 +2,7 @@ import math
 
 t_area= lambda b,h : 1/2*(b*h)
 r_area= lambda l,b : l*b
-c_area= lambda r : math.pi*r*r
+s_area= lambda a : a*a
 
 s=int(input("Enter the breadth of triangle : "))
 t=int(input("Enter the height of triangle : "))
@@ -10,5 +10,5 @@ print("Area of triangle : ",t_area(s,t))
 u=int(input("Enter the length of rectangle : "))
 v=int(input("Enter the breadth of rectangle : "))
 print("Area of rectangle : ",r_area(u,v))
-w=int(input("Enter the radius of circle : "))
-print("Area of circle : ",c_area(w))
+w=int(input("Enter the length of the sqaure : "))
+print("Area of square : ",s_area(w))
